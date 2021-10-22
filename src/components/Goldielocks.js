099b15d1b7341ded65c83d6bc588d8527b7a56c5
LD Bartholomew <LDBartholomew@yahoo.com>
2021-10-22 11:38:50 -0500

    still testing pushing

---
+++ src/components/Goldielocks.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { NavBar } from "./nav/NavBar"
+//import { ApplicationViews } from "./ApplicationViews"
+import "./Goldielocks.css"
+
+
+export const Goldielocks = () => (
+    <>
+    <NavBar />
+    {/* <ApplicationViews /> */}
+    </>
+    
+)
